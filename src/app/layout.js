64a681959cf0455d5969e7b5dfc978755b8796bd
Tush786 <tusharsapate34@gmail.com>
2024-09-04 13:12:44 +0530

    navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,38 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+// import AuthProvider from "@/providers/AuthProvider";
+import Navbar from "@/Component/navbar/Navbar";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/provider/TheamProvider";
+// import Footer from "@/Component/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Saga Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <AuthProvider> */}
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="containermain">
+                <div className="wrapper">
+                   <Navbar/>
+                  {children}
+                  {/* <Footer /> */}
+                </div>
+              </div>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        {/* </AuthProvider> */}
+      </body>
     </html>
-  )
+  );
 }
