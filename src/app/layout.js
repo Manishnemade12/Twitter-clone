8968c460c2677b3560452cc86b205ede3e08b4b1
Tush786 <tusharsapate34@gmail.com>
2024-09-06 13:46:35 +0530

    footer categorylist and feature list component added

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import Navbar from "@/Component/navbar/Navbar";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/provider/TheamProvider";
 import AuthProvider from "@/provider/AuthProvider";
-// import Footer from "@/Component/footer/Footer";
+import Footer from "@/Component/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
                 <div className="wrapper">
                    <Navbar/>
                   {children}
-                  {/* <Footer /> */}
+                  <Footer />
                 </div>
               </div>
             </ThemeProvider>
