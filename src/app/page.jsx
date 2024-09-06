7296c2894be9c1b4e0aed20d2f91efba8ab2304f
Tush786 <tusharsapate34@gmail.com>
2024-09-06 15:55:25 +0530

    Revert "categorylist added"
    This reverts commit 379ef547fc615b20b1f79a07cadb971dbd1d9209.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
  import Featured from "@/Component/featured/featured";
- import CategoryList from "@/Component/categoryList/CategoryList";
+// import CategoryList from "@/Component/categoryList/CategoryList";
 // import CategoryList from "@/Component/categoryList/CategoryList";
 // import Featured from "@/Component/featured/featured";
 // import CardList from "@/Component/cardList/CardList";
@@ -14,8 +14,8 @@ export default function Home({ searchParams }) {
   return (
     <div className={styles.containermain}>
         <Featured />
-      <CategoryList />
-      {/* <div className={styles.content}>
+      {/* <CategoryList />
+      <div className={styles.content}>
         <CardList page={page}/>
         <Menu /> 
       </div>  */}
