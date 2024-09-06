5393572e0f827613ce3e8e701b6205bba11edf09
Tush786 <tusharsapate34@gmail.com>
2024-09-06 14:10:41 +0530

    category list and features component removed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
-import Featured from "@/Component/featured/featured";
-import CategoryList from "@/Component/categoryList/CategoryList";
+// import Featured from "@/Component/featured/featured";
+// import CategoryList from "@/Component/categoryList/CategoryList";
 // import CategoryList from "@/Component/categoryList/CategoryList";
 // import Featured from "@/Component/featured/featured";
 // import CardList from "@/Component/cardList/CardList";
@@ -13,8 +13,8 @@ export default function Home({ searchParams }) {
 
   return (
     <div className={styles.containermain}>
-       <Featured />
-      <CategoryList />
+       {/* <Featured />
+      <CategoryList /> */}
       {/* <div className={styles.content}>
         <CardList page={page}/>
         <Menu /> 
